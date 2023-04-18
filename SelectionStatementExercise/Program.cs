71f1c957b90a145e64c53e39a9850c90c1582c98
AlissaStegall <alissaistegall@gmail.com>
2023-04-17 18:03:11 -0700

    completed exercise2

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject?");
+
+            string subject = Console.ReadLine();
+
+            switch(subject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("I hate math!");
+                    break;
+                case "science":
+                    Console.WriteLine("Science rocks!");
+                    break;
+                case "english":
+                    Console.WriteLine("To be, or not to be... english is a great choice.");
+                    break;
+                case "history":
+                    Console.WriteLine("Another lovely choice.");
+                    break;
+                case "lunch":
+                    Console.WriteLine("Not techniqually a subject but I have to agree.");
+                    break;
+                default:
+                    Console.WriteLine("I don't know that subject.");
+                    break;
+
+
+            }
+
+
         }
     }
 }
